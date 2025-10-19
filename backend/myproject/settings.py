25a50d18d629541f029ecb6deade19324769b803
Zakharii <zahariy2007z@gmail.com>
2025-10-19 20:21:43 +0300

    Connect to db

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lab3_python',
+        'USER': 'broccoli',
+        'PASSWORD': 'vegetables',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
