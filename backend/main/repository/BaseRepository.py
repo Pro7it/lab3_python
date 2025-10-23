e2bf2c360205119e1decda27fcbcb1a538501590
bird <neroforwork@gmail.com>
2025-10-23 18:04:37 +0300

    added  my sophisticated code

--- backend/main/repository/BaseRepository.py
+++ backend/main/repository/BaseRepository.py
@@ -1,14 +1,23 @@
-from abc import abstractmethod, ABC
+from abc import ABC
+
 
 class BaseRepository(ABC):
     def __init__(self, model):
         self.model = model
 
     def get_all(self):
-        pass
+        return list(self.model.objects.all())
 
     def get_by_id(self, id):
-        pass
+        try:
+            return self.model.objects.get(id=id)
+        except Exception:
+            return None
 
     def create(self, **kwargs):
-        pass
+        try:
+            e = self.model(**kwargs)
+            e.save()
+            return e
+        except Exception:
+            return None
