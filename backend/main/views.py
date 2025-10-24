c7e533b03392ff724e0f62654d469e10abbdea6b
Olena Kshyk <olena.kshyk.shi.2024@lpnu.ua>
2025-10-24 09:57:12 +0300

    deleting smth 2.0

--- backend/main/views.py
+++ backend/main/views.py
@@ -3,11 +3,3 @@ from django.shortcuts import render
 from .repository.MainPoint import MainPoint
 
 # Create your views here.
-def theatre_view(request):
-    return HttpResponse(MainPoint().theaters.get_by_id(request.GET.get('id')))
-
-def play_view(request):
-    return HttpResponse(MainPoint().plays.get_by_id(request.GET.get('id')))
-
-def genre_view(request):
-    return HttpResponse(MainPoint().genres.get_all())
