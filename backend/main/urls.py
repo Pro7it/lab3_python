d89390da0b7a6054574069338da52d3820697a94
Olena Kshyk <olena.kshyk.shi.2024@lpnu.ua>
2025-10-24 09:47:38 +0300

    deleting smth

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -2,7 +2,4 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('theatre/', theatre_view, name='theatre'),
-    path('play/', play_view, name='play'),
-    path('genre/', genre_view, name='genre'),
 ]
