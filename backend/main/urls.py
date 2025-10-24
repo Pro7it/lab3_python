e43b7f1e5bdaec85e4d5b85afe3080653298dd03
bird <neroforwork@gmail.com>
2025-10-24 11:37:12 +0300

    Magerovskii please dont bully us

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('<str:model_name>', get, name='get'),
+    path('<str:model_name>', get),
+    path('<str:model_name>/add', add),
 ]
