7ae0dc950534776bac4bb3bbc1f1ddb9ddf450b2
Zakharii <zahariy2007z@gmail.com>
2025-10-24 10:10:06 +0300

    Adding dynamic repository search

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('<str:model_name>', get, name='get'),
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 from .repository.MainPoint import MainPoint
 
 # Create your views here.
+def get(request, model_name):
+    mp = MainPoint()
+
+    if not hasattr(mp, model_name): return HttpResponse(f"Error: repository '{model_name}' not found")
+
+    repository = getattr(mp, model_name)
+    item_id = request.GET.get('id')
+
+    if item_id: return HttpResponse(str(repository.get_by_id(item_id)))
+    else: return HttpResponse(str(repository.get_all()))
